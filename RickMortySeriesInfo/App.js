0a593c983c2e0fad8631a93cd908fbb17e8e053e
Sergio Aragonés <sergioat7@gmail.com>
2020-06-21 19:13:55 +0200

    Created bottom tab bar with 3 classes

--- RickMortySeriesInfo/App.js
+++ RickMortySeriesInfo/App.js
@@ -1,114 +1,102 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import 'react-native-gesture-handler';
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Ionicons';
+import CharacterList from './src/components/CharacterList'
+import LocationList from './src/components/LocationList'
+import EpisodeList from './src/components/EpisodeList'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type Props = {};
+export default class App extends Component<Props> {
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  constructor(props) {
+    super(props);
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+    this.state = {
+      ready: false,
+    };
+  }
 
-export default App;
+  componentDidMount() {
+    this.createMainNavigation();
+  }
+
+  createMainNavigation() {
+
+    this.caractersStack = () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="Characters" component={CharacterList} />
+        </Stack.Navigator>
+      );
+    };
+    
+    this.locationsStack = () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="Location" component={LocationList} />
+        </Stack.Navigator>
+      );
+    };
+    
+    this.episodesStack = () => {
+      return (
+        <Stack.Navigator>
+          <Stack.Screen name="Episodes" component={EpisodeList} />
+        </Stack.Navigator>
+      );
+    };
+    
+    this.mainTab = () => {
+      return (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="characters"
+            component={this.caractersStack}
+            options={{
+              tabBarLabel: 'Characters',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name='ios-heart' size={size} color={color} />
+              ),
+            }} />
+          <Tab.Screen
+            name="locations"
+            component={this.locationsStack}
+            options={{
+              tabBarLabel: 'Locations',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name='ios-heart' size={size} color={color} />
+              ),
+            }} />
+          <Tab.Screen
+            name="episodes"
+            component={this.episodesStack}
+            options={{
+              tabBarLabel: 'Episodes',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name='ios-heart' size={size} color={color} />
+              ),
+            }} />
+        </Tab.Navigator>
+      );
+    }
+
+    this.navigationContainer = () => {
+      return (<NavigationContainer><this.mainTab /></NavigationContainer>);
+    };
+
+    this.setState({
+      ready: true,
+    });
+  }
+
+  render() {
+    return (this.state.ready ? <this.navigationContainer /> : <View />);
+  }
+}
