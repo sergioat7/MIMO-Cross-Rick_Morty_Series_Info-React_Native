76d187b454764428bae8bdd0aae348a0f04ecb6a
Sergio Aragonés <sergioat7@gmail.com>
2020-06-28 14:30:57 +0200

    Requested locations from server

--- RickMortySeriesInfo/src/components/EpisodeList.js
+++ RickMortySeriesInfo/src/components/EpisodeList.js
@@ -1,12 +1,79 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+    View,
+    StyleSheet,
+} from 'react-native';
+import RickAndMortyApiClient from '../api/RickAndMortyApiClient'
 
 export default class EpisodeList extends Component {
     
+    constructor(props) {
+        super(props);
+        
+        this.state = { locations: [] };
+        this.apiClient = new RickAndMortyApiClient();
+        this.nextPage = 1;
+        this.numberOfPages = 1;
+        this.isLoading = false;
+    }
+    
+    componentDidMount() {
+        this.loadNextPage()
+    }
+    
+    loadNextPage() {
+
+        if (this.nextPage > this.numberOfPages) {
+            return;
+        }
+        
+        if (this.isLoading) {
+            return;
+        }
+        
+        this.isLoading = true;
+        
+        this.loadPage(this.nextPage)
+            .then( ({resultLocations, numberOfPages}) => {
+            
+                let locations = resultLocations.map( location => {
+                    return {
+                        key: location.id.toString(),
+                        location: location
+                    }
+                })
+                this.setState({
+                    locations: this.state.locations.concat(locations)
+                });
+                this.nextPage++;
+                this.numberOfPages = numberOfPages;
+            })
+            .catch( error => {
+                console.error(error);
+            })
+            .finally( () => {
+                this.isLoading = false;
+            });
+    }
+    
+    loadPage(page) {
+        return this.apiClient.getLocations(page);
+    }
     
     render() {
+
         return (
-            <View />
+            <View style={styles.container} />
         );
     }
 }
+
+const styles = StyleSheet.create({
+    
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#f5fcff',
+    },
+});
